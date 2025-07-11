b951e8ebe8b0b299e270954d6155082fd29da212
AlvesArthur1983 <arturpatriciocaico@gmail.com>
2025-07-11 15:53:10 -0300

    Ultimos métodos RESTFUL com SQLModels

--- Aulas/aula06 - SQLModel/main.py
+++ Aulas/aula06 - SQLModel/main.py
@@ -7,23 +7,23 @@ from contextlib import asynccontextmanager
 urlsqlite = "sqlite:///banco.db"
 
 #Mysql - pymysql
-urlmysql = "mysql+pymysql://usuario:senha@localhost"
+urlmysql = "mysql+pymysql://usuario:senha@localhost/banco"
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(urlsqlite, connect_args=connect_args)
 
 def get_session():
     with Session(engine) as session:
-        yield session
+        yield session # Yield é usado para ficar retornando session igual a anterior, porém se não exitir, é criada outras
 
-SessionDep = Annotated[Session, Depends(get_session)]
+SessionDep = Annotated[Session, Depends(get_session)] #Variável usada para criar uma anotação de Alias e ficar recuperando o valor aberto
 
 
 def get_create_db():
-    SQLModel.metadata.create_all(engine)
+    SQLModel.metadata.create_all(engine) #Criação das tabelas baseados na Classe
 
-@asynccontextmanager
-async def lifespan(app:FastAPI):
+@asynccontextmanager 
+async def lifespan(app:FastAPI): #Async usado para não ser executado antes da criação da Aplicação, somente no meio dela
     get_create_db()
     yield
 
@@ -34,3 +34,28 @@ app = FastAPI(lifespan=lifespan)
 def alunos(session:SessionDep) -> List[Aluno]:
     alunos = session.exec(select(Aluno)).all()
     return alunos
+
+@app.post('/alunos')
+def cadastrar(session:SessionDep, aluno:Aluno)-> Aluno:
+    session.add(aluno)
+    session.commit()
+    session.refresh(aluno)
+    return aluno
+
+@app.delete("/alunos/{id}")
+def deletar(session:SessionDep, id:int)->str:
+    consulta = select(Aluno).where(Aluno.id==id)
+    aluno = session.exec(consulta).one()
+    session.delete(aluno)
+    session.commit()
+    return "Aluno foi de babau"
+
+@app.put("/alunos/{id}")
+def atualizar(session:SessionDep, id:int, nome:str) -> Aluno:
+    consulta = select(Aluno).where(Aluno.id==id)
+    aluno = session.exec(consulta).one()
+    aluno.nome = nome
+    session.add(aluno)
+    session.commit()
+    session.refresh(aluno)
+    return aluno
